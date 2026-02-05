ab6ff8badaf5260af0598c2b81f364ec0e1bdfd8
bullones <amandabullones@uma.es>
2026-02-05 13:17:29 +0100

    Updated README and configure files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # FunRichR
 
-
+A comprehensive R markdown pipeline based on [clusterProfiler](https://doi.org/10.1016/j.xinn.2024.100722) package for functional enrichment analysis. It can be used for model and non-model organisms.
 
 ***
 
@@ -22,16 +22,99 @@ Then you can navigate inside _FunRichR_ using shell comands or window interface.
 
 The GitHub repository and your local version will contain the following files and folders:
 
-- `README.md`: this file
--
+- `LICENSE`: details about the CC-BY license.
+- `README.md`: this file.
+- `configure.R`: a file containing all parameters necessary to execute _FunRichR_. This file can be located anywhere in your computer and is the only file that requires user intervention.
+- `enrichment.Rmd`: a markdown file where other Rmd fragments are incorporated.
+- `libraries.R`: a file that loads, installs and updates the required libraries.
+- `parsing.R`: a file that constructs a table with the necessary information for the reference organisms.
+- `Rmd`: a folder containing the Rmd fragments needed for the pipeline.
+
+***
+
+## Prerequisites and dependencies
+
+Minimum **RAM** required (depending on dataset size): 16 GB. Recommended RAM: 32 GB+
+
+_FunRichR_ has been developed with **R 4.5.2** on mac and linux computers. Older R releases my fail due to library obsolescence.
+
+**R packages/libraries** required are listed in `libraries.R`. Do not worry for that since on the first _FunRichR_ run, it will check which libraries are not installed and will install them automatically. It may take for a very long time (up to 30 min, depending on the number of packages to be installed).
+
+You should have some beginner experience with **bash** (Unix Shell) and R to execute this pipeline and transfer datasets to and from directories or computers.
 
 ***
 
 ## Quick Start
 
+_FunRichR_ can be launched using R console or RStudio in macOS, linux or Windows as
+
+```r
+source("~/PATH_TO/configure.R")
+```
+
+or using the terminal as
+
+```bash
+Rscript ~/PATH_TO/configure.R 
+```
+
+If you try this just after downloading _FunRichR_, it will not work because you have to accommodate it to your computer environment. So, read carefully the next section before launching _FunRichR_ for the first time.
+
 ***
 
 ## Configuration
+
+_FunRichR_ can be customised to your needs in the 'self-explained' `configure.R` file. Open this file and you will see an initial segment entitled `DON'T TOUCH: CLEAN START` marking the three commands that you must not modify. They will clean memory variables before a _FunRichR_ run.
+
+After that, you will find all customisable parameters with a detailed explanation and exemplary values. In the following list, 
+
+- **Compulsory** tag indicates that you have to customise the parameter to run _FunRichR_ for the first time or to read a specific experiment data.
+- **Recommended** parameters will facilitate your analysis, but are not necessary to run _FunRichR_ in your computer.
+- *Optional* parameters do not require customisation unless you detect a problem with your data. For example, you can have copies of the configure file with different set of optional parameters to compare the results.
+
+Parameters customisable in the `configure.R`:
+
+- `PKG_UPDATE`: `TRUE` if you want to update R packages before running _FunRichR_, or `FALSE`if you only want to install new packages. *Optional*
+- `DPI`: resolution for plots. *Optional*
+- `MY_TITLE`: the project name to easily identify your work. **Recommended**
+- `SOURCE_DIR`: the path to the directory of _FunRichR_. **Compulsory**
+- `MODEL_REFERENCE`: if your organism is one of the model references listed in the table. **Compulsory**
+    + When you set `MODEL_REFERENCE` = FALSE, you have to uncomment
+        - `ORG_ORIG`: provide a name to identify your organism. **Compulsory**
+- `REFERENCE_ORG`: choose between a list of model references for your enrichment analysis. **Compulsory**
+- `WD_DIR`: the path to the directory containing your data. **Compulsory**
+- `FILE`: data table with the genes of interest. **Compulsory**
+    + `EXPERIMENT`: choose an input file type from the list provided. *Compulsory*
+        - When you set `EXPERIMENT` = 3, you have to uncomment
+            + `FIRST_COND`: set a name for the first condition. **Compulsory**
+            + `SECOND_COND`: set a name for the second condition. **Compulsory**
+            + `COLFC`: define number of column with the Fold Change information. **Compulsory**
+- When you set `MODEL_REFERENCE` = FALSE, you have to uncomment
+    + `ORTHOLOGY_FILE`: provide the absolute path to your orthology file. **Compulsory**
+    + `REFERENCE_COLUMN`: column to read from `ORTHOLOGY_FILE` with the model reference. **Compulsory**
+    + `STUDY_COLUMN`: column to read from `ORTHOLOGY_FILE` with the study organism. **Compulsory**
+- `KEYTYPE`: define the format of your input genes. **Compulsory**
+- If you want to mark the transcription factors from your model reference, you have to uncomment
+    + `TF_FILE`: provide the absolute path to your transcription factors file. *Optional*
+- If you want to mark other important genes from your study organism, you have to uncomment
+    + `OTHER_FILE`: provide the absolute path to your file with genes of interest. *Optional*
+- `SHOW_CODE`: `TRUE` if you want the complete report showing code chunks, or `FALSE` if you want to hide (fold) code chunks. *Optional*
+- `DO_REMOVE_DUPS`: `TRUE` if you want genes that are duplicated in both sides of your comparison, or `FALSE` if you want to keep them. *Optional*
+- `P_ADJUST_METHOD`: statistical method used to adjust _P_-values. *Optional*
+- `P_CUTOFF`: minimal _P_-value for significance in any statistical analysis. *Optional*
+- `Q_CUTOFF`: minimal _Q_-value for significance in any statistical analysis. *Optional*
+- `NCLUST_USER`: indicate the number of clusters that you want to obtain for treeplots. *Optional*
+- `MIN_GENES`: minimum number of genes for a process to be plotted. *Optional*
+- `SIGNODES`: significant number of enriched GOs to be plotted for hierarchical trees. *Optional*
+- `MIN_GSSIZE_USER`: minimum number of genes that a GO term must have to be considered in the analysis. *Optional*
+- `MAX_GSSIZE_USER`: maximum number of genes that a GO term must have to be considered in the analysis. *Optional*
+- `DO_REMOVE_REDUNDANCY`: `TRUE` if you want to remove redundancy of enriched GO terms, or `FALSE` if you want to keep it. *Optional*
+- `GO_LEVEL`: GO level to filter the enrichment results by. *Optional*
+- `MAX_PLOT`: maximum number of processes to be plotted. *Optional*
+- `MAXTOPGOS`: maximum number of Top GOs to be plotted as an expression heatmap. *Optional*
+- `MIN_RATIO`: ratio to calculate the limit of the number of words that will be plotted in the wordclouds. *Optional*
+
+As a result of this `configure.R` file customisation, you can have as many copies of this file as you want (with the name you prefer) for different data or different parameters. These copies can be located wherever you want in your computer. 
 
 ***
 
--- configure.R
+++ configure.R
@@ -17,6 +17,9 @@ T00 <- proc.time()
 #
 # TRUE: old packages will be updated after installing the absent ones
 # FALSE: only absent packages will be installed. No update of older ones
+#
+# Example:
+#   PKG_UPDATE = FALSE
 
 PKG_UPDATE = FALSE
 # /////////////////////////////
@@ -29,7 +32,7 @@ PKG_UPDATE = FALSE
 # output. Margin: 72-1200
 #
 # Example:
-# DPI = 150
+#   DPI = 150
 
 DPI = 150
 # /////////////////////////
@@ -70,23 +73,28 @@ SOURCE_DIR = "~/datos_bullones/Bioinformatica/Script/Script_R/FunRichR/"
 # %%%%
 # TRUE: your study organism is on the list
 # 1. Arabidopsis thaliana
-# 2.Zebrafish
+# 2. Zebrafish
 # 3. Human
 # FALSE: your study organism in not on the list
+#
+# Example:
+#   MODEL_REFERENCE = FALSE
 
 MODEL_REFERENCE = FALSE
 
 # If your study organism if not a model reference, uncomment the ORG_ORIG line and provide a name to identify your organism in the result tables that will be generated
+# In the other case, comment the ORG_ORIG line
 #
 # Example:
 #   ORG_ORIG = "Olive"
 
-ORG_ORIG = "Picual"
+ORG_ORIG = "Olive"
+# ///////////////////////////////////////
 
-# %%%%
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # CHOOSE YOUR MODEL REFERENCE FOR ENRICHMENT ANALYSIS
-# %%%%
-# You must choose between:
+# Regardless of whether your organism is a model reference or not, you must choose between:
 # 1: Arabidopsis thaliana
 # 2: Zebrafish
 # 3: Human
@@ -109,7 +117,7 @@ REFERENCE_ORG = 1
 #   WD_DIR = "~/Documents/My_MA_data/this_experiment/"
 # A final "/" in path is compulsory
 
-WD_DIR = "~/datos_bullones/Bioinformatica/sequia_olivo_ana/data_26_1_23_both/new_analysis_06-11-23/RSeqFlow1.02_results_2024-01-11_12.52.04_koroneiki_cpm0_fc1.2_p0.05/"
+WD_DIR = "~/datos_bullones/Bioinformatica/data/"
 # //////////////////////////////////////
 
 
@@ -120,6 +128,7 @@ WD_DIR = "~/datos_bullones/Bioinformatica/sequia_olivo_ana/data_26_1_23_both/new
 # File must be in tsv format
 # File must be within the working directory (WD_DIR)
 # REQUIRED: the first column must contain the gene IDs
+#
 # Example:
 #   FILE = "my_file.tsv"
 
@@ -130,19 +139,37 @@ FILE = "AllGenes_allContrast_TREAT-P-0.05_FC-1.2_2024-01-11_12.52.04.tsv"
 # 2: gene IDs and cluster group
 # 3: gene IDs and log(foldchange)
 # 4: only gene IDs
+#
 # Example:
 #   EXPERIMENT = 1
 
 EXPERIMENT = 1
 
-# if your experiment is type 3, uncomment next lines
-# second condition
+# If you set EXPERIMENT = 3, uncomment the FIRST_COND, SECOND_COND, and COLFC lines
+# In the other case, comment FIRST_COND, SECOND_COND, and COLFC lines
+#
+# Set a name for the first condition
+#
+# Example:
+#   FIRST_COND = "AI15"
+
 # FIRST_COND = "AI15"
+
+# Set a name for the second condition
+#
+# Example:
+#   SECOND_COND = "CC15"
+
 # SECOND_COND = "CC15"
-# number of column with the Fold Change information
+
+# Define number of column with the Fold Change information
+#
+# Example:
+#   COLFC = 3
+
 # COLFC = 3
 
-# if your experiment is type 3 and you want to perform GSEA analyses, you must provide a full datatable with all the expressed genes and their log(foldchange) values (FILE), and a DEG list for the over-enrichment analyses (DEG-OE). Uncomment next line
+# If your experiment is type 3 and you want to perform GSEA analyses, you must provide a full datatable with all the expressed genes and their log(foldchange) values (FILE), and a DEG list for the over-enrichment analyses (DEG-OE). Uncomment next line
 # DEG_OE = "~/datos_bullones/Bioinformatica/depurar/DEGs.txt"
 # /////////////////////////
 
@@ -150,45 +177,61 @@ EXPERIMENT = 1
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # LOAD YOUR ORTHOLOGY TABLE ####
 #
-# In case your study organism is not a model organism, uncomment ORTHOLOGY_FILE and provide the absolute path to your orthology file
+# In case your study organism is not a model organism, uncomment ORTHOLOGY_FILE, REFERENCE_COLUMN, and STUDY_COLUMN lines
+# In the other case, comment ORTHOLOGY_FILE, REFERENCE_COLUMN, and STUDY_COLUMN lines
+#
+# Absolute path to your orthology file
 # File must be in tsv format
-# REQUIRED: the first column must be the study organism gene IDs
 # REQUIRED: the file MUST have a header
+#
 # Example:
 #   ORTHOLOGY_FILE = "~/Documents/orthology.tsv"
 
-ORTHOLOGY_FILE = "~/datos_bullones/Bioinformatica/ortologia/picual-arabidopsis_new_ort/picual-tair10/results/best_hit_two-way_picual_tair10.tsv"
+ORTHOLOGY_FILE = "~/datos_bullones/Bioinformatica/ortologia/best_hit_two-way_picual_tair10.tsv"
 
-# In case your study organism is not a model organism, uncomment REFERENCE_COLUMN and STUDY_COLUMN to specify the column to read with the reference and the study organisms
+# Specify the column to read from ORTHOLOGY_FILE with the model reference
+#
 # Example:
 #   REFERENCE_COLUMN = 2
-#   STUDY_COLUMN = 1
 
 REFERENCE_COLUMN = 2
-STUDY_COLUMN = 1
 
-# Choose the ID source (TAIR, ENTREZID, SYMBOL, ENSEMBL, etc)
+# Specify the column to read from ORTHOLOGY_FILE with the study organism
+#   STUDY_COLUMN = 1
+
+STUDY_COLUMN = 1
+# //////////////////////////////
+
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%
+# GENE IDENTIFIER FORMAT ####
+#
+
+# Choose the format of your input genes (TAIR, ENTREZID, SYMBOL, ENSEMBL, etc) for clusterProfiler to properly identify them
+# Use library(org.At.tair.db) and keytypes(org.At.tair.db), or your specific OrdDb (https://bioconductor.org/packages/release/BiocViews.html#___OrgDb) to list all supported options
+#
 # Example:
 #   KEYTYPE = "TAIR"
 
 KEYTYPE = "TAIR"
-# ///////////////////////////////////////
+# ///////////////////////////
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # TRANSCRIPTION FACTORS FOR STUDY ORGANISM ####
 #
-# You may want to mark the transcription factors from your reference organism. In this case, uncomment TF_FILE and specify the absolute path to your file
+# You may want to mark the transcription factors from your model reference. In this case, uncomment TF_FILE and specify the absolute path to your file
 # File must be in tsv format
 # REQUIRED: your file must have the following structure:
 # First column = TF ID
 # Second column = Gene ID
 # Third column = Family
-# REQUIRED: your TF and Gene ID columns must be of the reference organism
+# REQUIRED: your TF and Gene ID columns must be of the model reference
+#
 # Example:
 #   TF_FILE = "~/Documents/tf_file.tsv"
 
-TF_FILE = "~/datos_bullones/Bioinformatica/RNA_Seq/muestras_picual_Juande/Olivo4Plants/TF/Ath_TF_list.txt"
+TF_FILE = "~/datos_bullones/Bioinformatica/TF/Ath_TF_list.txt"
 # /////////////////////////////////////////////
 
 
@@ -201,10 +244,11 @@ TF_FILE = "~/datos_bullones/Bioinformatica/RNA_Seq/muestras_picual_Juande/Olivo4
 # First column = Identification tag
 # Second column = Gene ID
 # REQUIRED: your Gene ID column must be of the study organism
+#
 # Example:
 #   OTHER_FILE = "~/Documents/important_genes.tsv"
 
-OTHER_FILE = "~/datos_bullones/Bioinformatica/sequia_olivo_ana/data_26_1_23_both/new_analysis_06-11-23/RSeqFlow1.02_results_2024-01-11_12.52.04_koroneiki_cpm0_fc1.2_p0.05/OutstandingGenes-2024-01-11_12.52.04.tsv"
+OTHER_FILE = "~/datos_bullones/Bioinformatica/OutstandingGenes-2024-01-11_12.52.04.tsv"
 # //////////////////////////
 
 
@@ -213,6 +257,7 @@ OTHER_FILE = "~/datos_bullones/Bioinformatica/sequia_olivo_ana/data_26_1_23_both
 #
 # TRUE: R code is shown
 # FALSE: R code is hidden
+#
 # Example:
 #   SHOW_CODE = TRUE
 
@@ -226,6 +271,7 @@ SHOW_CODE = TRUE
 # In case you are studying DEGs, you may want to remove genes that are overexpressed and repressed at the same time in the same treatment-control analysis
 # TRUE: duplicate genes will be removed
 # FALSE: duplicate genes will not be removed
+#
 # Example:
 #   DO_REMOVE_DUPS = TRUE
 
@@ -236,7 +282,8 @@ DO_REMOVE_DUPS = TRUE
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # P-VALUE ADJUSTMENT METHOD ####
 #
-# You must specify the P-value adjustment method. Available options are: "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none"
+# You must select the statistical method used to adjust P-values,. Available options are: "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none"
+#
 # Example:
 #   P_ADJUST_METHOD = "BH"
 
@@ -248,6 +295,7 @@ P_ADJUST_METHOD = "BH"
 # THRESHOLD FOR P-VALUE ####
 #
 # You must specify the adjusted P-value cutoff on enrichment tests to report as significant
+#
 # Example:
 #   P_CUTOFF = 0.05
 
@@ -271,8 +319,9 @@ Q_CUTOFF = 0.2
 #
 # You can specify the number of clusters for treeplots as an integer
 # If you want that value to be automatically calculated, set it to 0
+# 
 # Example:
-#   NCLUST_USER = 4
+#   NCLUST_USER = 0
 
 NCLUST_USER = 0
 # ////////////////////////////
@@ -281,7 +330,9 @@ NCLUST_USER = 0
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # MINIMUM NUMBER OF GENES TO PLOT ####
 #
-# You need to set the minimum number of genes for a process to be plotted. You can set it to 0 if you want to plot the process regardless the number or genes. This parameter will only be applied to over-enrichment analyses.
+# You need to set the minimum number of genes for a process to be plotted
+# You can set it to 0 if you want to plot the process regardless the number or genes
+# This parameter will only be applied to over-enrichment analyses
 #
 # Example:
 #   MIN_GENES = 0
@@ -305,7 +356,10 @@ SIGNODES = 0
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # SIZE OF GENES FOR CLUSTERPROFILER ####
 #
-# You can set the minimal and maximal size of genes annotated for clusterProfiler analyses (over-enrichment, KEGG and GSEA). MINGSSIZE defines the minimum number of genes that a GO term must have to be considered in the analysis, discarding terms that are below. This avoids GO terms with too few genes, which may not be statistically significant or too specific. MAXGSSIZE defines the maximum number of genes that a GO term must have to be considered in the analysis, discarding terms that are above. This filters overly general GO terms (which include many genes). If you set the to 0, they will be automatically calculated.
+# You can set the minimal and maximal size of genes annotated for clusterProfiler analyses (over-enrichment, KEGG and GSEA).
+# MINGSSIZE defines the minimum number of genes that a GO term must have to be considered in the analysis, discarding terms that are below. This avoids GO terms with too few genes, which may not be statistically significant or too specific
+# MAXGSSIZE defines the maximum number of genes that a GO term must have to be considered in the analysis, discarding terms that are above. This filters overly general GO terms (which include many genes)
+# If you set them to 0, they will be automatically calculated.
 #
 # Example:
 #   MIN_GSSIZE_USER = 0
@@ -322,6 +376,7 @@ MAX_GSSIZE_USER = 0
 # You may want to remove redundancy of enriched GO terms.
 # TRUE: redundancy will be removed
 # FALSE: redundancy will not be removed
+# 
 # Example:
 #   DO_REMOVE_REDUNDANCY = TRUE
 
@@ -332,18 +387,22 @@ DO_REMOVE_REDUNDANCY = TRUE
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # WILL GO TERMS BE FILTERED? ####
 #
-# You may want to filter GO enriched results at specific GO level. That way you remove GO parent terms, which are more general terms within the GO hierarchy that group together more specific terms (children terms). Set the GO level you want to filter the enrichment by. Higher values will be closer to the children terms. If you set it to 0, no filter will be applied. Values between 1 and 7.
+# You may want to filter GO enriched results at specific GO level. That way you remove GO parent terms, which are more general terms within the GO hierarchy that group together more specific terms (children terms)
+# Set the GO level you want to filter the enrichment by. Higher values will be closer to the children terms
+# Values between 1 and 7
+# If you set it to 0, no filter will be applied
+#
 # Example:
 #   GO_LEVEL = 4
 
-GO_LEVEL = 4
+GO_LEVEL = 0
 # ///////////////////////////////
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # MAXIMUM PROCESSES TO PLOT ####
 #
-# You need to set the maximum number of processes to be plotted. Too large a value will cause problems when plotting the graphs. GSEA results will have up to twice as many processes as over-enrichment and KEGGs.
+# You need to set the maximum number of processes to be plotted. Too large a value will cause problems when plotting the graphs. GSEA results will have up to twice as many processes as over-enrichment and KEGGs
 #
 # Example:
 #   MAX_PLOT = 100
@@ -355,7 +414,8 @@ MAX_PLOT = 100
 # %%%%%%%%%%%%%%%%%%%%%%
 # MAXIMUM TOP GOS ######
 #
-# In the enrichment with enrichGO, we calculate the number of genes involved in a GO term in our dataset (Counts) and the number of genes involved in that process in the organism of interest (Background). We calculate the Counts/Background ratio to determine the most represented GOs (Top GOs). You must specify the maximum number of Top GOs to plot as an expression heatmap.
+# In the enrichment with enrichGO, we calculate the number of genes involved in a GO term in our dataset (Counts) and the number of genes involved in that process in the organism of interest (Background). We calculate the Counts/Background ratio to determine the most represented GOs (Top GOs). You must specify the maximum number of Top GOs to be plotted as an expression heatmap
+#
 # Example:
 #   MAXTOPGOS = 100
 
@@ -366,10 +426,11 @@ MAXTOPGOS = 100
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # FREQUENCY FOR WORDCLOUDS ####
 #
-# In order to set a limit on the number of words that will be plotted in the wordclouds according to their frequency of occurrence, you need to provide a MIN_RATIO to make those calculations. Bigger value implies more terms to plot.
+# In order to set a limit of the number of words that will be plotted in the wordclouds according to their frequency of occurrence, you need to provide a MIN_RATIO to make those calculations. Bigger value implies more terms to be plotted
 #                 Max frequency
 # Min frequency = -------------
 #                   MIN_RATIO
+#
 # Example:
 #   MIN_RATIO = 3
 
